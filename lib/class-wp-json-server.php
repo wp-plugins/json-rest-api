d797787476b4dc98988fba8acb822f7898d253c4
rmccue <rmccue@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-02 14:30:09 +0000

    Update to version 0.1.2
    git-svn-id: https://plugins.svn.wordpress.org/json-rest-api/trunk@735069 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/class-wp-json-server.php
+++ lib/class-wp-json-server.php
@@ -5,7 +5,7 @@
  * Contains the WP_JSON_Server class.
  *
  * @package WordPress
- * @version 0.1.1
+ * @version 0.1.2
  */
 
 require_once ABSPATH . 'wp-admin/includes/admin.php';
@@ -774,12 +774,12 @@ class WP_JSON_Server {
 		}
 
 		// Thumbnail
-		$post_fields_extended['post_thumbnail'] = array();
+		/*$post_fields_extended['post_thumbnail'] = array();
 		$thumbnail_id = get_post_thumbnail_id( $post['ID'] );
 		if ( $thumbnail_id ) {
 			$thumbnail_size = current_theme_supports( 'post-thumbnail' ) ? 'post-thumbnail' : 'thumbnail';
 			$post_fields_extended['post_thumbnail'] = $this->_prepare_media_item( get_post( $thumbnail_id ), $thumbnail_size );
-		}
+		}*/
 
 		// Consider future posts as published
 		if ( $post_fields['status'] === 'future' )
